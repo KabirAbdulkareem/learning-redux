f89c62923d295d2e7d6819d468004824b81c0571
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2020-04-20 13:09:51 +0100

    Created a directory for the store and moved the store implementation code from src/index to the store directory

--- src/index.js
+++ src/index.js
@@ -2,33 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import store from "./store/index"
 
-import { createStore, combineReducers } from "redux";
-
-const personReducer = (state = {}, { type, payload }) => {
-  if (type === "UPDATE_NAME") {
-    return { name: payload };
-  }
-  return state;
-};
-
-const gameReducer = (state = {}, { type, payload }) => {
-  if (type === "UPDATE_GAME") {
-    return { name: payload };
-  }
-  return state;
-};
-
-const AllReducers = combineReducers({
-  person: personReducer,
-  game: gameReducer,
-});
-
-const store = createStore(AllReducers);
-
-console.log(store.getState());
-
-store.dispatch({ type: "UPDATE_NAME", payload: "Daddy Maxwell" });
-console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById("root"));
